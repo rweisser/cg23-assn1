de3861736bc2a2834af4748e20a7f7427704e77e
rweisser <rweisser114@gmail.com>
2023-02-14 22:19:47 -0500

    add code for testing intercepts

--- trace/src/Trace.cpp
+++ trace/src/Trace.cpp
@@ -1,4 +1,4 @@
-// basic starter code for ray tracing assignment
+// Trace.cpp
 
 // For shared_data.hpp to work correctly, MAIN must be defined in this file,
 // but must not be defined in any other file.
@@ -21,20 +21,12 @@
 #include "Vec3.hpp"
 #include "parse.hpp"
 
+using namespace std;
+
 const char* usage = "Trace filename";
 
-using namespace std;
-int main(int argc, char **argv)
+void test_parse()
 {
-	if (argc != 2) {
-		cout << "usage: " << usage << endl;
-		exit(1);
-	}
-	char* file_name_arg = argv[1];
-	string file_name(PROJECT_DATA_DIR);
-	file_name += file_name_arg;
-	parse_file(file_name);
-	
 	cout << "background: " << background << endl;
 	cout << "eyep: " << eyep << endl;
 	cout << "lookp: " << lookp << endl;
@@ -47,8 +39,74 @@ int main(int argc, char **argv)
 	}
 	cout << endl;
 	for_each(sphere_vec.cbegin(), sphere_vec.cend(), [](const Sphere& s) { cout << s << endl; });
+}
 
-	return 0;
+void test_intersect2(Vec3& e, Vec3& d, Sphere& s)
+{
+	cout << endl;
+	cout << "e = " << e << ", d = " << d << ", s = " << s << endl;
+	double t1 = -1000000, t2 = -1000000;
+	bool res = s.intersect(e, d, t1, t2);
+	cout << "length of d - e is " << (d - e).mag() << endl;
+	cout << "result = " << res
+		<< ", t1 = " << t1
+		<< ", t2 = " << t2 << endl;
+	cout << "color = " << surface_map[s.surface_name].color << endl;
+}
+
+void test_intersect()
+{
+	cout << "starting test_intersect" << endl;
+
+	test_intersect2(
+		Vec3(1, 1, 0),  // e
+		Vec3(1, 0, 0),  // d
+		Sphere("sph001", 1, Vec3(4, 1, 0))); // s
+
+	test_intersect2(
+		Vec3(1, 1, 0),  // e
+		Vec3(1, 0, 0),  // d
+		Sphere("sph001", 3, Vec3(4, 1, 0))); // s
+
+	test_intersect2(
+		Vec3(1, 1, 0),  // e
+		Vec3(1, 0, 0),  // d
+		Sphere("sph001", 5, Vec3(4, 1, 0))); // s
+
+	test_intersect2(
+		Vec3(1, 1, 1),  // e
+		Vec3(2, 2, 2),  // d
+		Sphere("sph001", 1, Vec3(3, 3, 3))); // s
+
+	test_intersect2(
+		Vec3(1, 1, 1),  // e
+		Vec3(2, 2, 2),  // d
+		Sphere("sph001", 0.5, Vec3(3, 3, 3))); // s
+
+	test_intersect2(
+		Vec3(1, 1, 1),  // e
+		Vec3(2, 2, 2),  // d
+		Sphere("sph001", 0.5, Vec3(20, 20, 20))); // s
+
+	test_intersect2(
+		Vec3(1, 1, 1),  // e
+		Vec3(2, 2, 2),  // d
+		Sphere("sph001", 0.5, Vec3(20, 0, 0))); // s
+}
+
+int main(int argc, char **argv)
+{
+	if (argc != 2) {
+		cout << "usage: " << usage << endl;
+		exit(1);
+	}
+	char* file_name_arg = argv[1];
+	string file_name(PROJECT_DATA_DIR);
+	file_name += file_name_arg;
+	parse_file(file_name);
+
+	// test_parse(); return 0;
+	test_intersect();
 
 	// platform-independent timing[
     auto startTime = std::chrono::high_resolution_clock::now();
