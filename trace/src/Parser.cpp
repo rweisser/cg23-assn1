08d2f426ca00aaedb81f432c07690afa98789ab9
rweisser <rweisser114@gmail.com>
2023-06-07 09:38:44 -0400

    create Parser class to replace parse.cpp

---
+++ trace/src/Parser.cpp
@@ -0,0 +1,102 @@
+// ===========================================================================
+// Parser.cpp
+
+// A parser for ray trace files
+// ===========================================================================
+
+using namespace std;
+
+#include "G.hpp"
+#include "Parser.hpp"
+
+// ===========================================================================
+// Public member functions
+// ===========================================================================
+
+Parser::Parser(G g) : g(g) {}
+
+void Parser::parse_file(const string& file_name)
+{
+    ray_file.open(file_name);
+    if (!ray_file) {
+        cerr << file_name << ": ";
+        cerr << strerror(errno) << endl;
+        exit(1);
+    }
+    read_file();                // process the file
+    ray_file.close();
+}
+
+// ===========================================================================
+// Private member functions
+// ===========================================================================
+
+void Parser::read_file()
+{
+    // read a keyword
+    // ignore tokens we don't care about
+    string token;
+    while (1) {
+        ray_file >> token;
+        if (ray_file.eof())
+            break;
+        if (token == "background")
+            parse_vec3(g.background);
+        else if (token == "eyep")
+            parse_vec3(g.eyep);
+        else if (token == "lookp")
+            parse_vec3(g.lookp);
+        else if (token == "up")
+            parse_vec3(g.up);
+        else if (token == "fov")
+            parse_vec2(g.fov);
+        else if (token == "screen")
+            parse_vec2(g.screen_size);
+        else if (token == "surface")
+            parse_surface();
+        else if (token == "sphere")
+            parse_sphere();
+    }
+}
+
+// parse background, fov, and screen
+void Parser::parse_vec2(Vec2& v) {
+    double x, y;
+    ray_file >> x >> y;
+    v = { x, y };
+}
+
+// parse background color, eyep, lookp, and up
+void Parser::parse_vec3(Vec3& v) {
+    double x, y, z;
+    ray_file >> x >> y >> z;
+    v = { x, y, z };
+}
+
+void Parser::parse_surface()
+{
+    string name;
+    string token;
+    double r, x, y, z;
+
+    ray_file >> name;
+    while (1) {
+        ray_file >> token;
+        if (token == "diffuse") {
+            ray_file >> x >> y >> z;
+            Surface s(name, { x, y, z });
+            surface_map.insert({ s.name, s });
+            return;
+        }
+    }
+}
+
+void Parser::parse_sphere()
+{
+    string surface_name;
+    double radius, x, y, z;
+    ray_file >> surface_name >> radius >> x >> y >> z;
+    Vec3 color = surface_map[surface_name].color;
+    Sphere s(surface_name, radius, Vec3(x, y, z), color);
+    g.sphere_vec.push_back(s);
+}
---
+++ trace/src/Parser.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+// ===========================================================================
+// Parser.hpp
+
+// A parser for ray trace files
+// ===========================================================================
+
+using namespace std;
+
+#include <cerrno>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include "G.hpp"
+
+class Parser
+{
+    G&       g;           // global vars
+    string   file_name;   // name of ray trace file
+    ifstream ray_file;    // ray trace file
+    unordered_map<string, Surface> surface_map;
+
+    void parse_vec2(Vec2& v);
+    void parse_vec3(Vec3& v);
+    void parse_sphere();
+    void parse_surface();
+    void read_file();
+
+public:
+    Parser(G g);
+
+    // Parse the ray trace file into variables in g, which
+    // contains global variables.  Since g is a reference, the
+    // changes will be there after the Parser object is gone.
+    void parse_file(const string& file_name);
+};
