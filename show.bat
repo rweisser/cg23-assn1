a613696e286fa3a8fce098923b9d936065492929
rweisser <rweisser114@gmail.com>
2023-08-17 11:07:35 -0400

    modify show.bat and trace.bat to use new output directory

--- show.bat
+++ show.bat
@@ -1,1 +1,8 @@
-trace\build\trace.ppm
+@echo off
+rem Show the output ppm file in the output directory.
+
+if %1 == "" (
+    echo usage: show filename
+    goto :EOF
+)
+trace\output\trace.ppm %1
--- trace.bat
+++ trace.bat
@@ -1,9 +1,14 @@
 @echo off
 rem Run the trace program and show the output ppm file
 
+if %1 == "" (
+    echo usage: trace filename
+    goto :eof
+)
 trace\build\Release\trace.exe %1
 if %errorlevel% neq 0 (
     echo Trace failed
     goto :eof
 )
-show
+copy trace\build\%1 trace\output\%1
+show %1
